43f595be34f689a5141ad47a78c34e36be52c24a
LeeChiehYin <ha24847@bristol.ac.uk>
2024-11-15 00:09:51 +0000

    create file for vendor class

---
+++ Vendor.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 23:36:28 2024
+
+@author: Chieh-Yin
+"""
+
+#Vendor class
+class Vendor:
+    def __init__(self, name, fertilizer_price, feed_price, salt_price):
+        self.name = name
+        self.fertilizer_p = fertilizer_p
+        self.feed_p = feed_p 
+        self.salt_p = salt_p
+        
+SL = Vendor('Slippery Lakes', 0.3, 0.1, 0.05)
+SW = Vendor('Scaly Wholescaler', 0.2, 0.4, 0.25) 

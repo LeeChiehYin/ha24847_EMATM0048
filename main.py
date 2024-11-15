115d78dd8d132beb956d2b6aa9699db4e2af403e
LeeChiehYin <ha24847@bristol.ac.uk>
2024-11-15 01:00:50 +0000

    mod file(not ok)

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ Created on Thu Nov 14 19:29:42 2024
 #Quarter, Technicains
 quarter = int(input('Please entr number of quarters:'))
 tech_count = 0
+tech_list = []
 
 for quarter_count in range(1, quarter + 1):
     print('{:=^50s}'.format('STIMULATING quarter'+str(quarter_count)))
@@ -19,18 +20,37 @@ for quarter_count in range(1, quarter + 1):
     #Technician
     while True:
         try:
-            tech = int(input('To add enter positive, to remove enter negtive, no change enter 0.\n>>>Please enter number of technicians:')
-            if 1<= tech_count+tech <=5:
-                tech_count +=tech_count
-        
-            print('Number of technicains', tech_count)
-            if tech_count < 1:
+            tech = int(input('To add enter positive, to remove enter negtive, no change enter 0.\n>>>Please enter number of technicians:'))
+            
+            new_tech_count = tech_count + tech #先建一個new避免規定外呃數值被算入正式tech           
+            if new_tech_count < 1:
                 print('You need tat least 1 technicains in tis quarter.')
             elif tech_count > 5:
                 print('Sorry, you cannot hire more than 5 technicians.')
             else:
-                break
-         except ValueError:
+                if tech > 0: # 當新增的tech為正數，詢問新進tech name
+                    for i in range(tech):
+                        name_add = input('Please enter name of new technician.')
+                        tech_list.append(name_add)
+                        break
+                elif tech < 0: #當新增的tech為負數，詢問要移除的tech_name
+                   for i in range(abs(tech)):
+                       if tech_list:
+                           name_remove = input("Please enter the name of the technicians you'd like to remove")
+                           if name_remove in tech_list:
+                               tech_list.remove(name_remove)
+                           else: #如果name_remove不在list
+                               print('Technician', name_remove, 'is not in the Technician list. Please check the name again!')
+                       else: #如果list是空的
+                           print('The Technician list is empty!')
+                           break
+                       
+                   tech_count + new_tech_count
+                   print('Current number of technicians:', tech_count)
+                   print('Current Technicain list:', tech_list)
+                   break
+           
+        except ValueError:
              print('Please enter an integer number.')
              
 print(tech_count)
